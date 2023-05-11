6bac1feacac56b514763c01aef7472bff13583a3
Uladzimir Khotchanka <hotchenko@gmail.com>
2023-05-11 12:49:50 +0200

    update index template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -131,11 +131,11 @@
                     <div class="text-center text-sm text-gray-500 sm:text-left">
                         <div class="flex items-center">
                             <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400">
-                                <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                                <path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                             </svg>
 
-                            <a href="https://www.stimulsoft.com/online-store" class="ml-1 underline">
-                                Stimulsoft Shop
+                            <a href="https://www.stimulsoft.com/en/products/reports-php" class="ml-1 underline">
+                                Stimulsoft Reports.PHP
                             </a>
 
                             <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="ml-4 -mt-px w-5 h-5 text-gray-400">
