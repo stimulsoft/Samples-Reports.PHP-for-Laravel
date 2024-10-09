cff2411dde9bac62a49d69b7bea3eff30f1e27ad
Uladzimir Khotchanka <hotchenko@gmail.com>
2024-10-09 13:09:19 +0200

    update component templates for 2024.4.1 release

--- resources/views/designer.blade.php
+++ resources/views/designer.blade.php
@@ -1,70 +1,24 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <title>Stimulsoft Reports.PHP - Designer</title>
-    <style>
-        html, body {
-            font-family: sans-serif;
-        }
-    </style>
+<?php
+use Stimulsoft\Designer\StiDesigner;
+use Stimulsoft\Report\StiReport;
 
-    <?php
-    /** https://www.stimulsoft.com/en/documentation/online/programming-manual/index.html?reports_and_dashboards_for_php_web_designer_deployment.htm */
-    $js = new \Stimulsoft\StiJavaScript(\Stimulsoft\StiComponentType::Designer);
-    $js->renderHtml();
-    ?>
 
-    <script type="text/javascript">
-        <?php
-        $handler = new \Stimulsoft\StiHandler();
-        $handler->options->url = '/handler';
-        //$handler->license->setKey('6vJhGtLLLz2GNviWmUTrhSqnO...');
-        //$handler->license->setFile('license.key');
-        $handler->renderHtml();
+// Creating a designer object
+$designer = new StiDesigner();
 
-        /** https://www.stimulsoft.com/en/documentation/online/programming-manual/index.html?reports_and_dashboards_for_php_web_designer_settings.htm */
-        $options = new \Stimulsoft\Designer\StiDesignerOptions();
-        $options->appearance->fullScreenMode = true;
+// Processing the request and, if successful, immediately printing the result
+$designer->process();
 
-        /** https://www.stimulsoft.com/en/documentation/online/programming-manual/index.html?reports_and_dashboards_for_php_web_designer_deployment.htm */
-        $designer = new \Stimulsoft\Designer\StiDesigner($options);
+// Creating a report object
+$report = new StiReport();
 
-        /** https://www.stimulsoft.com/en/documentation/online/programming-manual/index.html?reports_and_dashboards_for_php_engine_connecting_sql_data.htm */
-        $designer->onBeginProcessData = true;
+// Loading a report by URL
+// This method does not load the report object on the server side, it only generates the necessary JavaScript code
+// The report will be loaded into a JavaScript object on the client side
+$report->loadFile('reports/SimpleList.mrt');
 
-        /** https://www.stimulsoft.com/en/documentation/online/programming-manual/index.html?reports_and_dashboards_for_php_web_designer_saving_report.htm */
-        $designer->onSaveReport = true;
+// Assigning a report object to the designer
+$designer->report = $report;
 
-        /** https://www.stimulsoft.com/en/documentation/online/programming-manual/index.html?reports_and_dashboards_for_php_web_designer_creating_editing_report.htm */
-        $report = new \Stimulsoft\Report\StiReport();
-        $report->loadFile('reports/SimpleList.mrt');
-        $report->onBeforeRender = "onBeforeRender";
-
-        /*$var1 = new \Stimulsoft\Report\StiVariable('var1', \Stimulsoft\Report\StiVariableType::String, "abc");
-        $report->dictionary->variables[] = $var1;
-
-        $var2 = new \Stimulsoft\Report\StiVariable('var2', \Stimulsoft\Report\StiVariableType::Decimal, 4.34);
-        $report->dictionary->variables[] = $var2;*/
-
-        $designer->report = $report;
-        ?>
-
-        function onBeforeRender(args) {
-            let var1 = new Stimulsoft.Report.Dictionary.StiVariable('', 'var1', 'var1', '', Stimulsoft.System.Decimal, '4.34');
-            args.report.dictionary.variables.add(var1);
-        }
-
-        // After loading the HTML page, display the visual part of the Designer in the specified container.
-        function onLoad() {
-            <?php
-            $designer->renderHtml('designerContent');
-            ?>
-        }
-    </script>
-</head>
-<body onload="onLoad();">
-<div id="designerContent"></div>
-</body>
-</html>
+// Displaying the visual part of the designer as a prepared HTML page
+$designer->printHtml();
--- resources/views/viewer.blade.php
+++ resources/views/viewer.blade.php
@@ -1,60 +1,24 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <title>Stimulsoft Reports.PHP - Viewer</title>
-    <style>
-        html, body {
-            font-family: sans-serif;
-        }
-    </style>
-    <?php
-    /** https://www.stimulsoft.com/en/documentation/online/programming-manual/index.html?reports_and_dashboards_for_php_deployment.htm */
-    $js = new \Stimulsoft\StiJavaScript(\Stimulsoft\StiComponentType::Viewer);
-    $js->renderHtml();
-    ?>
+<?php
+use Stimulsoft\Report\StiReport;
+use Stimulsoft\Viewer\StiViewer;
 
-    <script type="text/javascript">
-        <?php
-        $handler = new \Stimulsoft\StiHandler(false);
-        $handler->options->url = '/handler';
-        //$handler->license->setKey('6vJhGtLLLz2GNviWmUTrhSqnO...');
-        //$handler->license->setFile('license.key');
-        $handler->renderHtml();
 
-        /** https://www.stimulsoft.com/en/documentation/online/programming-manual/index.html?reports_and_dashboards_for_php_settings.htm */
-        $options = new \Stimulsoft\Viewer\StiViewerOptions();
-        $options->toolbar->showSendEmailButton = true;
-        $options->toolbar->displayMode = \Stimulsoft\Viewer\StiToolbarDisplayMode::Separated;
-        $options->appearance->fullScreenMode = true;
-        $options->appearance->scrollbarsMode = true;
-        $options->height = '600px'; // Height for non-fullscreen mode
+// Creating a viewer object
+$viewer = new StiViewer();
 
-        /** https://www.stimulsoft.com/en/documentation/online/programming-manual/index.html?reports_and_dashboards_for_php_deployment.htm */
-        $viewer = new \Stimulsoft\Viewer\StiViewer($options);
+// Processing the request and, if successful, immediately printing the result
+$viewer->process();
 
-        /** https://www.stimulsoft.com/en/documentation/online/programming-manual/index.html?reports_and_dashboards_for_php_engine_connecting_sql_data.htm */
-        $viewer->onBeginProcessData = true;
+// Creating a report object
+$report = new StiReport();
 
-        /** https://www.stimulsoft.com/en/documentation/online/programming-manual/index.html?reports_and_dashboards_for_php_web_viewer_send_email.htm */
-        $viewer->onEmailReport = true;
+// Loading a report by URL
+// This method does not load the report object on the server side, it only generates the necessary JavaScript code
+// The report will be loaded into a JavaScript object on the client side
+$report->loadFile('reports/SimpleList.mrt');
 
-        /** https://www.stimulsoft.com/en/documentation/online/programming-manual/index.html?reports_and_dashboards_for_php_web_designer_creating_editing_report.htm */
-        $report = new \Stimulsoft\Report\StiReport();
-        $report->loadFile('reports/SimpleList.mrt');
-        $viewer->report = $report;
-        ?>
+// Assigning a report object to the viewer
+$viewer->report = $report;
 
-        // After loading the HTML page, display the visual part of the Viewer in the specified container.
-        function onLoad() {
-            <?php
-            $viewer->renderHtml('viewerContent');
-            ?>
-        }
-    </script>
-</head>
-<body onload="onLoad();">
-<div id="viewerContent"></div>
-</body>
-</html>
+// Displaying the visual part of the viewer as a prepared HTML page
+$viewer->printHtml();
